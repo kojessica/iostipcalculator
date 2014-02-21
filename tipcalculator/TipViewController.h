d6c16b76e1aef9f980612332e2cb44ff2cc35ecb
Jessica Ko <jessicako@jessicako-macbookpro.roam.corp.google.com>
2014-02-20 17:49:56 -0800

    first commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jessica Ko on 2/20/14.
+//  Copyright (c) 2014 Jessica Ko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/settingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  settingsViewController.h
+//  tipcalculator
+//
+//  Created by Jessica Ko on 2/20/14.
+//  Copyright (c) 2014 Jessica Ko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface settingsViewController : UIViewController
+
+@end
